89be58d3194adc41d72a608308cf1ed34e2cf3a8
Tivawo <tvw203@gmail.com>
2023-03-29 12:50:55 +0200

    CI/CD Tests fix #4

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestDemo {
     @Test
     public void demoTest() {
-        assertTrue(true);
+        int i = 2;
+        i += 3;
+        assertEquals(5, i);
     }
 }
