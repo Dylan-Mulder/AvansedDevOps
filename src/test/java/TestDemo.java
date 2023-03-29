814f0c65813986c0b3e1bac0304da6060cd05bef
Tivawo <tvw203@gmail.com>
2023-03-29 12:27:20 +0200

    CI/CD Tests hotfix

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DemoTests {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestDemo {
     @Test
     public void demoTest() {
         assertTrue(true);
